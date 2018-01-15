122c454d90e534ca7386eca24d3e535d318dd391
laowang <vipkia@sina.cn>
2018-01-15 22:01:04 +0800

    改为通过搜索问题返回结果条数

--- answer_partner.py
+++ answer_partner.py
@@ -1,52 +1,47 @@
 import os
 import pytesseract
 import time
+import requests
+
 from PIL import Image
-from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 def screen_shots():
+    time1 = time.clock()
     os.system('adb shell screencap -p /sdcard/autosearch.png')
     os.system('adb pull /sdcard/autosearch.png .')
+    print('截图：' + str(time.clock() - time1))
 
 
 def screen_pic():
-    screen_shots()
+    # screen_shots()
+    time1 = time.clock()
     img = Image.open('autosearch.png')
     w, h = img.size
     # 依据图片尺寸定的截图区间
-    im1 = img.crop((0, h // 10, w, h // 4))
-    print(h // 3)
+    im1 = img.crop((0, h // 10, w, h // 3 * 2))
     im1.save('autosearch.png')
+    print('裁剪：' + str(time.clock() - time1))
 
 
 def query_sub():
-    # screen_pic()
+    screen_pic()
     image = Image.open('autosearch.png')
     code = pytesseract.image_to_string(image, lang='chi_sim')
-    print('题目是：{}'.format(code.replace('\n', '').split('.')[1].strip(' ')))
-
-    # 搜索网站的地址
-    # url = 'https://www.google.com'
-    url = 'http://www.baidu.com'
-    # chromedriver.exe安装位置
-    browser = webdriver.Chrome('E:\\soft\\chrome\\chromedriver.exe')
-    # 输入url
-    browser.get(url)
-    # 休眠1s
-    # time.sleep(1)
-    # 谷歌搜索
-    # browser.find_element_by_id('lst-ib').clear()
-    # # 去除字符两边空格以及题目序号
-    # browser.find_element_by_id('lst-ib').send_keys(code.replace('\n', '').split('.')[1].strip(' '))
-    # browser.find_element_by_name('btnK').click()
-
-    # 百度搜索
-    browser.find_element_by_id('kw').clear()
-    # 去除字符两边空格以及题目序号
-    browser.find_element_by_id('kw').send_keys(code.replace('\n', '').split('.')[1].strip(' '))
-    browser.find_element_by_id('su').click()
-    print('搜索成功!')
+    code = code.replace(' ', '').split('\n\n')
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0) Gecko/20100101 Firefox/44.0'}
+    url = 'http://www.baidu.com/s'
+    question = code[0].replace('\n', '').split('.')[1]
+    print(question)
+    for ans in code[1:]:
+        payload = question + '  ' + ans
+        payload = {'wd': payload}
+        result = requests.get(url=url, params=payload, headers=headers)
+        soup = BeautifulSoup(result.text, 'html.parser')
+        tags = str(soup.find_all('div', class_='nums')[0])
+        res = list(filter(str.isdigit, tags))
+        print('{}:[{}]'.format(ans, ''.join(res)))
 
 
 if __name__ == '__main__':
